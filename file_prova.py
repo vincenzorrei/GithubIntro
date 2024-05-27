0ebde27ac8adf9e854ea67a7b31710d87b4519cd
vincenzorrei <vin.orrei@gmail.com>
2024-05-27 18:05:37 +0200

    New Commit

---
+++ file_prova.py
@@ -0,0 +1,11 @@
+import random
+
+questions = [
+    "What is the capital of France?",
+    "Who painted the Mona Lisa?",
+    "What is the largest planet in our solar system?",
+    "Who wrote the novel 'Pride and Prejudice'?",
+]
+
+random_question = random.choice(questions)
+print(random_question)
